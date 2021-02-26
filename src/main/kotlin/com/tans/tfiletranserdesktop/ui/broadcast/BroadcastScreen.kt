d1eb1a8313c672105da1780673889e80fadff900
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2021-02-26 12:26:09 +0800

    Add image resource.

--- src/main/kotlin/com/tans/tfiletranserdesktop/ui/broadcast/BroadcastScreen.kt
+++ src/main/kotlin/com/tans/tfiletranserdesktop/ui/broadcast/BroadcastScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- src/main/kotlin/com/tans/tfiletranserdesktop/ui/filetransfer/MyFolderContent.kt
+++ src/main/kotlin/com/tans/tfiletranserdesktop/ui/filetransfer/MyFolderContent.kt
@@ -1,9 +1,29 @@
 package com.tans.tfiletranserdesktop.ui.filetransfer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.vectorXmlResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FileTransferScreen.MyFolderContent() {
-    Text("My Folder")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text("My Folder")
+        }
+
+        Box(modifier = Modifier.align(Alignment.BottomEnd).padding(20.dp)) {
+            FloatingActionButton(onClick = {}) {
+                Image(imageVector = vectorXmlResource("images/share_variant_outline.xml"), contentDescription = null)
+            }
+        }
+    }
 }
