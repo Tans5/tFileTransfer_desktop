635d023ba47d2399456786ed04c7c368770120e8
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2021-03-01 14:47:04 +0800

    Add message transfer.

--- src/main/kotlin/com/tans/tfiletranserdesktop/ui/filetransfer/MessageContent.kt
+++ src/main/kotlin/com/tans/tfiletranserdesktop/ui/filetransfer/MessageContent.kt
@@ -1,17 +1,21 @@
 package com.tans.tfiletranserdesktop.ui.filetransfer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.vectorXmlResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tans.tfiletranserdesktop.rxasstate.subscribeAsState
@@ -65,60 +69,62 @@ class MessageContent(
                         .align(Alignment.BottomCenter)
                         .fillMaxWidth()
                 ) {
-                    val messagesState = bindState().map { it.messages }.distinctUntilChanged().subscribeAsState(emptyList())
-                    val messages = messagesState.value
-                    LazyColumn(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        items(
-                            count = messages.size
-                        ) { index ->
-                            val message = messages[index]
-                            Box(
-                                modifier = Modifier.fillMaxWidth().padding(
-                                    start = 20.dp,
-                                    end = 20.dp,
-                                    top = 10.dp,
-                                    bottom = 10.dp
-                                )
-                            ) {
-                                if (message.isRemote) {
-                                    Row(Modifier.fillMaxWidth()) {
-                                        Card(
-                                            shape = RoundedCornerShape(10.dp),
-                                            backgroundColor = colorTeal700
-                                        ) {
-                                            SelectionContainer {
-                                                Text(
-                                                    text = message.message,
-                                                    style = TextStyle(
-                                                        color = colorWhite,
-                                                        fontSize = 14.sp
-                                                    ),
-                                                    modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 8.dp, bottom = 8.dp)
-                                                )
+                    Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
+                        val messagesState = bindState().map { it.messages }.distinctUntilChanged().subscribeAsState(emptyList())
+                        val messages = messagesState.value
+                        LazyColumn(
+                            modifier = Modifier.fillMaxWidth().align(Alignment.BottomEnd)
+                        ) {
+                            items(
+                                count = messages.size
+                            ) { index ->
+                                val message = messages[index]
+                                Box(
+                                    modifier = Modifier.fillMaxWidth().padding(
+                                        start = 20.dp,
+                                        end = 20.dp,
+                                        top = 10.dp,
+                                        bottom = 10.dp
+                                    )
+                                ) {
+                                    if (message.isRemote) {
+                                        Row(Modifier.fillMaxWidth()) {
+                                            Card(
+                                                shape = RoundedCornerShape(10.dp),
+                                                backgroundColor = colorTeal700
+                                            ) {
+                                                SelectionContainer {
+                                                    Text(
+                                                        text = message.message,
+                                                        style = TextStyle(
+                                                            color = colorWhite,
+                                                            fontSize = 14.sp
+                                                        ),
+                                                        modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 8.dp, bottom = 8.dp)
+                                                    )
+                                                }
                                             }
+                                            Spacer(modifier = Modifier.weight(1f))
+                                            Spacer(modifier = Modifier.width(70.dp))
                                         }
-                                        Spacer(modifier = Modifier.weight(1f))
-                                        Spacer(modifier = Modifier.width(70.dp))
-                                    }
-                                } else {
-                                    Row(Modifier.fillMaxWidth()) {
-                                        Spacer(modifier = Modifier.width(70.dp))
-                                        Spacer(modifier = Modifier.weight(1f))
-                                        Card(
-                                            shape = RoundedCornerShape(10.dp),
-                                            backgroundColor = colorWhite
-                                        ) {
-                                            SelectionContainer {
-                                                Text(
-                                                    text = message.message,
-                                                    style = TextStyle(
-                                                        color = colorTextBlack,
-                                                        fontSize = 14.sp
-                                                    ),
-                                                    modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 8.dp, bottom = 8.dp)
-                                                )
+                                    } else {
+                                        Row(Modifier.fillMaxWidth()) {
+                                            Spacer(modifier = Modifier.width(70.dp))
+                                            Spacer(modifier = Modifier.weight(1f))
+                                            Card(
+                                                shape = RoundedCornerShape(10.dp),
+                                                backgroundColor = colorWhite
+                                            ) {
+                                                SelectionContainer {
+                                                    Text(
+                                                        text = message.message,
+                                                        style = TextStyle(
+                                                            color = colorTextBlack,
+                                                            fontSize = 14.sp
+                                                        ),
+                                                        modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 8.dp, bottom = 8.dp)
+                                                    )
+                                                }
                                             }
                                         }
                                     }
@@ -126,9 +132,46 @@ class MessageContent(
                             }
                         }
                     }
+                    Divider(modifier = Modifier.fillMaxWidth().height(1.dp))
+                    Row(modifier = Modifier.fillMaxWidth().height(75.dp).background(color = colorWhite)
+                        .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp)) {
+                        val input = remember { mutableStateOf("") }
+                        OutlinedTextField(
+                            value = input.value,
+                            onValueChange = { newValue -> input.value = newValue },
+                            modifier = Modifier.fillMaxHeight().weight(1f),
+                            placeholder = {
+                                Text("Input Message.")
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                        )
+                        Spacer(Modifier.width(10.dp))
+                        IconButton(
+                            onClick = {
+                                launch {
+                                    val inputLocal = input.value
+                                    if (inputLocal.isNotBlank()) {
+                                        fileTransferScreen.fileTransporter.startWriterHandleWhenFinish(
+                                            newSendMessageShareWriterHandle(inputLocal)
+                                        )
+                                        updateState { oldState ->
+                                            val newMessage = Message(
+                                                isRemote = false,
+                                                timeMilli = System.currentTimeMillis(),
+                                                message = inputLocal
+                                            )
+                                            oldState.copy(messages = oldState.messages + newMessage)
+                                        }.await()
+                                    }
+                                    input.value = ""
+                                }
+                            },
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        ) {
+                            Image(imageVector = vectorXmlResource("images/send.xml"), contentDescription = null)
+                        }
+                    }
                 }
-                Divider(modifier = Modifier.fillMaxWidth().height(1.dp))
-
             }
         }
     }
