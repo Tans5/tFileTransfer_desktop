350fd84d5e50610f7b7269f47fd9744d246c32d1
tans.tan <810555251@qq.com>
2022-04-05 14:21:58 +0800

    Adjust buffer size.

--- src/main/kotlin/com/tans/tfiletranserdesktop/net/netty/filetransfer/FileSender.kt
+++ src/main/kotlin/com/tans/tfiletranserdesktop/net/netty/filetransfer/FileSender.kt
@@ -35,7 +35,7 @@ import java.util.concurrent.atomic.AtomicLong
 
 
 // 128KB
-private const val WRITE_BUFFER_SIZE = 1024 * 128
+private const val WRITE_BUFFER_SIZE = 1024 * 256
 private const val BUFFER_SIZE = 40
 
 val fileTransporterPool = NetBufferPool(
